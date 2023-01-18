e9355f8cb2af86d62524848a6ecc6abfe982b026
Kagweitis <kagweitis@gmail.com>
2023-01-18 16:22:01 +0300

    homepage

---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="nav">
+    <div class="logo">
+      <i src="../assets/Finder-1.png" alt="logo here" style="height: 100px; width: 100px;"></i>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+.nav{
+  background-color: red;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+    <Nav />
         <h2>Welcome </h2>
         <h1>You are home</h1>
         <button @click="handleSignOut">sign out</button>
@@ -10,6 +11,7 @@
 import { async } from "@firebase/util";
 import { getAuth, signOut } from "firebase/auth";
 import { useRouter } from "vue-router";
+import Nav from "../components/Nav.vue";
 
 const auth = getAuth();
 
