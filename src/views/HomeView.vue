679649089e86164c4af4358cd41ae4c7a61e720f
Kagweitis <kagweitis@gmail.com>
2023-01-24 16:22:58 +0300

    homepage work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,29 @@
 <template>
-    <div id="main">
-        <h2>Welcome </h2>
-        <div class="occupationInput">
-            <label>What</label>
-            <input type="text" placeholder="occupation" v-model="queryDetails.occupation">
-            <i class="fa-solid fa-briefcase"></i>
-        </div>
-        <div class="locationInput">
-            <label>Where</label>
-            <input type="text" placeholder="location" v-model="queryDetails.location">
-            <i class="fa-sharp fa-solid fa-location-dot"></i>
-        </div>
-        <button @click="search"> Search </button>
-        
-        <button @click="handleSignOut">sign out</button>
+  <div id="main">
+    <h2>Welcome</h2>
+    <div id="search-div">
+      <div class="occupationInput">
+        <label>What</label>
+        <input type="text" placeholder="job" v-model="queryDetails.occupation" />
+        <span class="fa-solid fa-briefcase"></span>
+      </div>
+      <div class="locationInput">
+        <label>Where</label>
+        <input
+          type="text"
+          placeholder="location"
+          v-model="queryDetails.location"
+        />
+        <i class="fa-sharp fa-solid fa-location-dot"></i>
+      </div>
+      <div>
+        <button @click="search">Search</button>
+      </div>
     </div>
+    <div>
+      <button @click="handleSignOut">sign out</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -25,35 +34,65 @@ import { reactive } from "vue";
 
 const auth = getAuth();
 
-const router = useRouter()
+const router = useRouter();
 
 const queryDetails = reactive({
-    occupation: '',
-    location: ''
-})
+  occupation: "",
+  location: "",
+});
 
 const search = (queryDetails) => {
-    console.log('search' ,queryDetails);
-}
+  console.log("search", queryDetails);
+};
 
 const handleSignOut = async () => {
-    await auth.signOut()
-    router.push('/')
-    
-}
+  await auth.signOut();
+  router.push("/");
+};
 </script>
 
 <style scoped>
-
-.occupationInput{
-    position: relative;
+#main {
+  display: flex;
+  font-family: 'Quicksand', sans-serif;
 }
 
-.occupationInput i{
-    position: absolute;
-    top: 50%;
-    right: 10px;
-    transform: translateY(-50%);
+#search-div {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  align-items: center;
+  padding: 4vh;
+  background-color: red;
 }
 
+
+input[type="text"] {
+  width: 20rem;
+  height: 2rem;
+  font-size: 20px;
+  border-radius: 10px;
+  margin-right: 2vh;
+}
+
+.occupationInput {
+  position: relative;
+}
+
+.occupationInput i {
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+}
+
+.fa-briefcase {
+  position: absolute;
+  left: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+/* input{
+    margin-left: 4vh;
+} */
 </style>
