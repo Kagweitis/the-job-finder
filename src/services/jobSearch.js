679a1563c2570a76f03dc3860d03c4807d00e7f6
Kagweitis <kagweitis@gmail.com>
2023-02-09 14:53:32 +0300

    search functionality

--- src/services/jobSearch.js
+++ src/services/jobSearch.js
@@ -4,13 +4,16 @@ async function searchJobs({occupation, jobLocation}) {
   const query = `${occupation}`;
   const location = `${jobLocation}`;
   const projectId = 'job-finder-377208';
+  const uuid = require('uuid');
+  const session = uuid.v4();
+
 
   const response = await axios.post('https://jobs.googleapis.com/v3/projects/{projectId}/jobs:search', {
     searchMode: 'JOB_SEARCH',
     requestMetadata: {
-      userId: 'YOUR_USER_ID',
-      sessionId: 'YOUR_SESSION_ID',
-      domain: 'YOUR_DOMAIN'
+      userId: 'kyk0VOBgbcheNrrBEyDSm0pWIy32',
+      sessionId: session,
+      domain: 'http://localhost:5173/'
     },
     jobQuery: {
       query: query,
