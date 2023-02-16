93281a00b53691e97366604c23d9fdc2e94d3e70
Kagweitis <Kagweitis@gmail.com>
2023-02-16 10:51:14 +0300

    get jobs implementation

--- src/services/jobSearch.js
+++ src/services/jobSearch.js
@@ -1,14 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios'
+
+
 
 async function searchJobs({occupation, jobLocation}) {
   const API_KEY = 'AIzaSyB4gszottgllCEjDGPkZEB2WwDzRuQ9XZg';
   const query = `${occupation}`;
   const location = `${jobLocation}`;
-  const projectId = 'job-finder-377208';
-  const uuid = require('uuid');
-  const session = uuid.v4();
+  const session = uuidv4();
+  console.log(session);
+  // const projectId = 'job-finder-377208';
+  // const uuid = require('uuid');
+  // const session = uuid.v4();
 
 
-  const response = await axios.post('https://jobs.googleapis.com/v3/projects/{projectId}/jobs:search', {
+  const response = await axios.post('https://jobs.googleapis.com/v3/projects/job-finder-377208/jobs:search', {
     searchMode: 'JOB_SEARCH',
     requestMetadata: {
       userId: 'kyk0VOBgbcheNrrBEyDSm0pWIy32',
